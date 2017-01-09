32be33d2fa4a2526131286fb0847099ec3417f87
Antonio Aguilar <aa6446694@gmail.com>
2017-01-09 13:06:48 +0000

    added support for HTTPS

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Simple file server with multipart/form-data and large file support
 ## Install 
 
 ```bash
-$ npm install -g upload-server
+npm install -g upload-server
 ```
 
 ## Usage
 
 ```bash
-$ upload-server
+upload-server
 ```
 
 This starts the server on the current host ```0.0.0.0``` on port ```8090```. Files are uploaded to ```./uploads``` under the current folder.
@@ -19,16 +19,35 @@ This starts the server on the current host ```0.0.0.0``` on port ```8090```. Fil
 #### Configure upload FOLDER
 
 ```bash
-$ FOLDER=./downloads upload-server
+FOLDER=./downloads upload-server
 ```
 
 #### Configure HOST and PORT
 
 ```bash
-$ HOST=192.168.0.131 PORT=8080 upload-server
+HOST=192.168.0.131 HTTP_PORT=8080 upload-server
 ```
 
-## AngularJS example
+### Configure server to use HTTPS
+
+Generate private key (key.pem) and server certificate (cert.pem) and place the files in the folder from which the server is run. You can use the following command to generate both the key and certificate:
+
+```bash
+openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -keyout key.pem -out cert.pem
+```
+
+Once you have the key and certificate, you can run the server using the following command:
+
+```bash
+upload-server --https
+```
+
+This starts the server on the current host ```0.0.0.0``` using HTTPS port ```8443```.
+
+
+### Uploading files
+
+This code snippet shows how to upload a file using AngularJS:
 
 ```javascript
 function uploadFile(content, filename) {
@@ -39,7 +58,7 @@ function uploadFile(content, filename) {
 
   $http({
     method: 'POST',
-    url: 'localhost:8090/upload',
+    url: '//localhost:8090/upload',
     data: form,
     headers: { 'Content-Type': undefined }
   }).then(function success(response) {}, function failure(error) {});
--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 
+var args = process.argv.slice(2);
 var host = process.env.HOST || '0.0.0.0';
-var port = process.env.PORT || 8090;
+var http_port = process.env.HTTP_PORT || 8090;
+var https_port = process.env.HTTPS_PORT || 8443;
 var folder = process.env.FOLDER || process.cwd() + '/uploads';
 
 var fs = require('fs');
+var http = require('http');
+var https = require('https');
 var express = require('express');
 var multer = require('multer');
 var app = express();
+var options = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
 
 fs.existsSync(folder) || fs.mkdirSync(folder);
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     cb(null, folder);
   },
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     var fieldName = 'file';
     req.body[fieldName] ? cb(null, req.body[fieldName]) : cb(null, file.originalname);
   }
@@ -23,21 +31,28 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('<form action="/upload" method="POST" enctype="multipart/form-data">\n  <input type="file" name="file">\n  <input type="submit" value="Upload File">\n</form>\n');
 });
 
-app.post('/upload', upload.any(), function (req, res) {
+app.post('/upload', upload.any(), function(req, res) {
   console.log('[' + new Date() + '] - File uploaded:', req.files[0].path);
   res.end();
 });
 
-app.listen(port, host, function () {
-  console.log('[' + new Date() + '] - File Upload Server started on ' + host + ':' + port);
-});
+if(args[0] === '--https') {
+  https.createServer(options, app).listen(https_port, host, function() {
+    console.log('[' + new Date() + '] - HTTPS File Upload Server started on ' + host + ':' + https_port);
+  });
+}
+else {
+  http.createServer(app).listen(http_port, host, function() {
+    console.log('[' + new Date() + '] - HTTP File Upload Server started on ' + host + ':' + http_port);
+  });
+}
